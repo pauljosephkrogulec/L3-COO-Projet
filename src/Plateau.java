4220dba0824166f050c53503c22f6098ff702702
pauljosephkrogulec <pauljoseph.travail@gmail.com>
2021-03-03 18:22:10 +0100

    estAtteignable full fonctionnelle

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,17 +1,22 @@
-import java.util.Stack;
+import java.lang.annotation.Retention;
+import java.security.Principal;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+
+import javax.management.Query;
 
 public class Plateau {
     private Couloir[][] couloirs;
+
     public Plateau(Couloir[] couloirs) {
-        this.couloirs=new Couloir[7][7];
-        int x =0;
-        int objs=0;
-        for(int i=0;i<7;i++){
-            for(int j=0;j<7;i++){
-                if(i%2==0 && j%2==0){
-                    this.couloirs[i][j]=new CouloirFixe(Orientation.SUD, Forme.TE , Objectif.values()[objs++]);
-                } else{
-                    this.couloirs[i][j]=couloirs[x++];
+        this.couloirs = new Couloir[7][7];
+        int objs = 0;
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (i % 2 == 0 && j % 2 == 0) {
+                    this.couloirs[i][j] = new CouloirFixe(Orientation.EST, Forme.TE, Objectif.values()[objs++]);
+                } else {
+                    this.couloirs[i][j] = new CouloirMobile(Orientation.SUD,Forme.DROIT,null,true);
                 }
             }
         }
@@ -22,47 +27,95 @@ public class Plateau {
         Position oPosition = pos.oppose().getPos();
         CouloirMobile opo = (CouloirMobile) this.couloirs[oPosition.getX()][oPosition.getY()];
         int x;
-        if (position.getX() == 0) for(x=0;x<6;x++) this.couloirs[position.getX()+x][position.getY()] =  this.couloirs[position.getX()+x+1][position.getY()];
-        else if (position.getY() == 0) for(x=0;x<6;x++) this.couloirs[position.getX()][position.getY()+x] =  this.couloirs[position.getX()][position.getY()+x+1];
-        else if (position.getX() == 6) for(x=0;x<6;x++) this.couloirs[position.getX()-x][position.getY()] =  this.couloirs[position.getX()-(x+1)][position.getY()];
-        else for(x=0;x<6;x++) this.couloirs[position.getX()][position.getY()-x] =  this.couloirs[position.getX()][position.getY()-(x+1)];
+        if (position.getX() == 0)
+            for (x = 0; x < 6; x++)
+                this.couloirs[position.getX() + x][position.getY()] = this.couloirs[position.getX() + x + 1][position
+                        .getY()];
+        else if (position.getY() == 0)
+            for (x = 0; x < 6; x++)
+                this.couloirs[position.getX()][position.getY() + x] = this.couloirs[position.getX()][position.getY() + x
+                        + 1];
+        else if (position.getX() == 6)
+            for (x = 0; x < 6; x++)
+                this.couloirs[position.getX() - x][position.getY()] = this.couloirs[position.getX() - (x + 1)][position
+                        .getY()];
+        else
+            for (x = 0; x < 6; x++)
+                this.couloirs[position.getX()][position.getY() - x] = this.couloirs[position.getX()][position.getY()
+                        - (x + 1)];
         this.couloirs[position.getX()][position.getY()] = c;
         return opo;
     }
 
-    private boolean estAdjacent(int a,int b) {
-        if(a > b) return estAdjacent(b, a);
-        Couloir ca,cb;
-        ca = this.couloirs[a/7][a%7];
-        cb = this.couloirs[b/7][b%7];
-        if((b%7) - (a%7) == 1) return (((ca.getForme() == Forme.COUDE && (ca.getOrientation() == Orientation.EST || ca.getOrientation() == Orientation.NORD)) || (ca.getForme() == Forme.DROIT && (ca.getOrientation() == Orientation.EST || ca.getOrientation() == Orientation.OUEST)) || (ca.getForme() == Forme.TE && (ca.getOrientation() == Orientation.EST || ca.getOrientation() == Orientation.NORD || ca.getOrientation() == Orientation.SUD))) && (cb.getForme() == Forme.COUDE && (cb.getOrientation() == Orientation.OUEST && cb.getOrientation() == Orientation.SUD)) || (cb.getForme() == Forme.DROIT && (cb.getOrientation() == Orientation.EST || cb.getOrientation() == Orientation.OUEST)) || (cb.getForme() == Forme.TE && (cb.getOrientation() == Orientation.OUEST || cb.getOrientation() == Orientation.NORD || cb.getOrientation() == Orientation.SUD)));
-        else if(b-7==a) return (((ca.getForme() == Forme.COUDE && (ca.getOrientation() == Orientation.EST && ca.getOrientation() == Orientation.SUD)) || (ca.getForme() == Forme.DROIT && (ca.getOrientation() == Orientation.SUD || ca.getOrientation() == Orientation.NORD)) || (ca.getForme() == Forme.TE && (ca.getOrientation() == Orientation.EST || ca.getOrientation() == Orientation.OUEST || ca.getOrientation() == Orientation.SUD))) && (cb.getForme() == Forme.COUDE && (cb.getOrientation() == Orientation.OUEST || cb.getOrientation() == Orientation.NORD)) || (cb.getForme() == Forme.DROIT && (cb.getOrientation() == Orientation.SUD || cb.getOrientation() == Orientation.NORD)) || (cb.getForme() == Forme.TE && (cb.getOrientation() == Orientation.EST || cb.getOrientation() == Orientation.OUEST || cb.getOrientation() == Orientation.NORD)));
+    private boolean estAdjacent(int a, int b) {
+        if (a > b)
+            return estAdjacent(b, a);
+        Couloir ca, cb;
+        if (b - a == 1) {
+            ca = this.couloirs[a / 7][a % 7];
+            cb = this.couloirs[b / 7][b % 7];
+            return (((ca.getForme() == Forme.COUDE
+                    && (ca.getOrientation() == Orientation.EST || ca.getOrientation() == Orientation.NORD))
+                    || (ca.getForme() == Forme.DROIT
+                            && (ca.getOrientation() == Orientation.EST || ca.getOrientation() == Orientation.OUEST))
+                    || (ca.getForme() == Forme.TE && (ca.getOrientation() == Orientation.EST
+                            || ca.getOrientation() == Orientation.NORD || ca.getOrientation() == Orientation.SUD)))
+                    && (cb.getForme() == Forme.COUDE
+                            && (cb.getOrientation() == Orientation.OUEST && cb.getOrientation() == Orientation.SUD))
+                    || (cb.getForme() == Forme.DROIT
+                            && (cb.getOrientation() == Orientation.EST || cb.getOrientation() == Orientation.OUEST))
+                    || (cb.getForme() == Forme.TE && (cb.getOrientation() == Orientation.OUEST
+                            || cb.getOrientation() == Orientation.NORD || cb.getOrientation() == Orientation.SUD)));
+        }
+        else if (b - 7 == a) {
+            ca = this.couloirs[a / 7][a % 7];
+            cb = this.couloirs[b / 7][b % 7];
+            return (((ca.getForme() == Forme.COUDE && (ca.getOrientation() == Orientation.EST && ca.getOrientation() == Orientation.SUD))
+                    || (ca.getForme() == Forme.DROIT && (ca.getOrientation() == Orientation.SUD || ca.getOrientation() == Orientation.NORD))
+                    || (ca.getForme() == Forme.TE && (ca.getOrientation() == Orientation.EST || ca.getOrientation() == Orientation.OUEST || ca.getOrientation() == Orientation.SUD)))
+                    && 
+                    (cb.getForme() == Forme.COUDE && (cb.getOrientation() == Orientation.OUEST || cb.getOrientation() == Orientation.NORD))
+                    || (cb.getForme() == Forme.DROIT && (cb.getOrientation() == Orientation.SUD || cb.getOrientation() == Orientation.NORD))
+                    || (cb.getForme() == Forme.TE && (cb.getOrientation() == Orientation.EST || cb.getOrientation() == Orientation.OUEST || cb.getOrientation() == Orientation.NORD)));
+        }
         return false;
     }
-    private boolean[][] matriceAdjacence(){
+
+    public boolean[][] matriceAdjacence() {
         boolean[][] m = new boolean[49][49];
-        for(int a = 0;a < 49; a++)
-            for(int b = 0;b < 49; b++)
+        for (int a = 0; a < 49; a++)
+            for (int b = 0; b < 49; b++)
                 m[a][b] = estAdjacent(a, b);
         return m;
     }
-    private boolean parcoursLargeur(int ox,int dx,int dy){
+
+    public boolean estAtteignable(Position orig, Position dest) {
+        ArrayDeque<Integer> pos = new ArrayDeque<>();
+        ArrayDeque<Integer> vPos = new ArrayDeque<>();
+        int x;
+        pos.add(orig.getX());
         boolean[][] m = this.matriceAdjacence();
-        for(int y=0;y < 49; y++){
-            if(m[ox][y]){
-                if(ox==dx && y==dy) return true;
-                if(parcoursLargeur(y, dx, dy)) return true;
+        while(pos.size() != 0){
+            x = pos.pollFirst();
+            vPos.add(x);
+            for(int y = 0; y<49;y+=1){
+                if(m[x][y]){
+                    System.out.print(x + "-> " + y + " ");
+                    if (!vPos.contains(y)) pos.add(y);
+                    if (y == (dest.getY() + (dest.getX() * 7))) return true; 
+                }
             }
+            
+            System.out.println();
         }
         return false;
-    } 
-    public boolean estAtteignable(Position orig, Position dest) {
-        return parcoursLargeur(orig.getX(), dest.getX(), dest.getY());
     }
 
     public Objectif deplacer(Position pos, Pion pion) {
         this.couloirs[pos.getX()][pos.getY()].setPions(pion);
         return this.couloirs[pos.getX()][pos.getY()].getObjectif();
     }
-    
+
+
 }
+
