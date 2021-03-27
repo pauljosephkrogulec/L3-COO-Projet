aa787c515c56aa3a82e1928556d0a9162d418fc6
Sowtch <quentin.cptr@gmail.com>
2021-03-27 16:35:22 +0100

    update

---
+++ src/DemoLabyrinthe.java
@@ -0,0 +1,8 @@
+/** Classe DemoLabyrinthe qui s'occupe de lancer le jeu.
+ */
+public class DemoLabyrinthe {
+    public static void main(String[] args) {
+        // creer le jeu du labyrinthe
+        new JeuLabyrinthe();
+    }
+} // DemoLabyrinthe
