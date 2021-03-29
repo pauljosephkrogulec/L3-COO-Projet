ce2d5700c4f29206b9f8b0fa20646dd3ecf1f4be
Sowtch <quentin.cptr@gmail.com>
2021-03-30 00:00:11 +0200

    update

--- src/Vue/VJeu.java
+++ src/Vue/VJeu.java
@@ -1,7 +1,6 @@
 package Vue;
 import java.util.List;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
 
@@ -19,8 +18,9 @@ public class VJeu extends JFrame {
     // On déclare quelques variables...
     private static int LONGUEUR = 800;
     private static int HAUTEUR = 500;
-    private JPanel labyrinthe, plateau;
     private VMenu menu;
+    private VLabyrinthe labyrinthe;
+    private JPanel plateau;
     private static List<Couloir> listBtn;
     private JeuImpl jeu;
 
@@ -36,9 +36,9 @@ public class VJeu extends JFrame {
 
         this.jeu = (JeuImpl) JeuFactory.creeJeu(this);
         
-        this.labyrinthe = new JPanel();
-        this.creerLabyrinthe();
+        this.labyrinthe = new VLabyrinthe(HAUTEUR);
         this.getContentPane().add(this.labyrinthe, "West");
+        this.plateau = creerPlateau();
 
         // On paramètre la fenêtre.
         this.setLocationRelativeTo(null);
@@ -58,105 +58,25 @@ public class VJeu extends JFrame {
 
     /** Méthode qui créer le Menu du jeu.
      */
-    public void creerLabyrinthe() {
-
-        this.labyrinthe.setBackground(new Color(68, 79, 116));
-        this.labyrinthe.setPreferredSize(new Dimension(500, HAUTEUR));
-        this.labyrinthe.setLayout(new BorderLayout());
-
-        JPanel top = new JPanel();
-        top.setBackground(new Color(68, 79, 116));
-        top.setPreferredSize(new Dimension(500, 50));
-        top.setLayout(null);
-
-        JButton btn0 = new InsertionTop(0);
-        btn0.setBounds(120, 10, 32, 32);
-        top.add(btn0);
-
-        JButton btn1 = new InsertionTop(0);
-        btn1.setBounds(235, 10, 32, 32);
-        top.add(btn1);
-
-        JButton btn2 = new InsertionTop(0);
-        btn2.setBounds(348, 10, 32, 32);
-        top.add(btn2);
-
-        this.labyrinthe.add(top, BorderLayout.NORTH);
-        
-        JPanel left = new JPanel();
-        left.setBackground(new Color(68, 79, 116));
-        left.setPreferredSize(new Dimension(50, 500));
-        left.setLayout(null);
-
-        JButton btn3 = new InsertionLeft(0);
-        btn3.setBounds(10, 71, 32, 32);
-        left.add(btn3);
-
-        JButton btn4 = new InsertionLeft(0);
-        btn4.setBounds(10, 185, 32, 32);
-        left.add(btn4);
-
-        JButton btn5 = new InsertionLeft(0);
-        btn5.setBounds(10, 300, 32, 32);
-        left.add(btn5);
-        
-        this.labyrinthe.add(left, BorderLayout.WEST);
-        
-        JPanel right = new JPanel();
-        right.setBackground(new Color(68, 79, 116));
-        right.setPreferredSize(new Dimension(50, 500));
-        right.setLayout(null);
-
-        JButton btn6 = new InsertionRight(0);
-        btn6.setBounds(10, 71, 32, 32);
-        right.add(btn6);
-
-        JButton btn7 = new InsertionRight(0);
-        btn7.setBounds(10, 185, 32, 32);
-        right.add(btn7);
-
-        JButton btn8 = new InsertionRight(0);
-        btn8.setBounds(10, 300, 32, 32);
-        right.add(btn8);
-
-        this.labyrinthe.add(right, BorderLayout.EAST);
-        
-        JPanel bottom = new JPanel();
-        bottom.setBackground(new Color(68, 79, 116));
-        bottom.setPreferredSize(new Dimension(500, 50));
-        bottom.setLayout(null);
-
-        JButton btn9 = new InsertionBottom(0);
-        btn9.setBounds(120, 10, 32, 32);
-        bottom.add(btn9);
-
-        JButton btn10 = new InsertionBottom(0);
-        btn10.setBounds(235, 10, 32, 32);
-        bottom.add(btn10);
-
-        JButton btn11 = new InsertionBottom(0);
-        btn11.setBounds(348, 10, 32, 32);
-        bottom.add(btn11);
-
-        this.labyrinthe.add(bottom, BorderLayout.SOUTH);
-               
-        this.plateau = new JPanel();
-        this.plateau.setBackground(new Color(68, 79, 116));
-        this.plateau.setLayout(null);
-        this.createJbutton();
+    public JPanel creerPlateau() {
+                       
+        JPanel p = new JPanel();
+        p.setBackground(new Color(68, 79, 116));
+        p.setLayout(null);
+        this.rempliLabyrinthe(p);
         GridLayout grid = new GridLayout(7,7);
-        this.plateau.setLayout(grid);
-        
-        this.labyrinthe.add(this.plateau, BorderLayout.CENTER);
+        p.setLayout(grid);
 
+        this.labyrinthe.add(p, BorderLayout.CENTER);
+
+        return p;
     }
 
-    private void createJbutton() {
+    private void rempliLabyrinthe(JPanel p) {
 
         listBtn = jeu.couloirs();
-        // add JButtons dynamically
         for (int i = 0; i < listBtn.size(); i++) {
-            this.plateau.add((JButton) listBtn.get(i));
+            p.add((JButton) listBtn.get(i));
         }
     }
 
@@ -164,7 +84,7 @@ public class VJeu extends JFrame {
         this.plateau.removeAll();
         this.plateau.revalidate();
         this.plateau.repaint();
-        this.createJbutton();
+        this.rempliLabyrinthe(this.plateau);
         this.plateau.revalidate();   
         this.plateau.repaint();
     }
---
+++ src/Vue/VLabyrinthe.java
@@ -0,0 +1,97 @@
+package Vue;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class VLabyrinthe extends JPanel {
+
+    public VLabyrinthe(int H) {
+        super();
+        
+        this.setBackground(new Color(68, 79, 116));
+        this.setPreferredSize(new Dimension(500, H));
+        this.setLayout(new BorderLayout());
+
+        this.creerLabyrinthe();
+    }
+
+    private void creerLabyrinthe() {
+        JPanel top = new JPanel();
+        top.setBackground(new Color(68, 79, 116));
+        top.setPreferredSize(new Dimension(500, 50));
+        top.setLayout(null);
+
+        JButton btn0 = new InsertionTop(0);
+        btn0.setBounds(120, 10, 32, 32);
+        top.add(btn0);
+
+        JButton btn1 = new InsertionTop(0);
+        btn1.setBounds(235, 10, 32, 32);
+        top.add(btn1);
+
+        JButton btn2 = new InsertionTop(0);
+        btn2.setBounds(348, 10, 32, 32);
+        top.add(btn2);
+
+        this.add(top, BorderLayout.NORTH);
+        
+        JPanel left = new JPanel();
+        left.setBackground(new Color(68, 79, 116));
+        left.setPreferredSize(new Dimension(50, 500));
+        left.setLayout(null);
+
+        JButton btn3 = new InsertionLeft(0);
+        btn3.setBounds(10, 71, 32, 32);
+        left.add(btn3);
+
+        JButton btn4 = new InsertionLeft(0);
+        btn4.setBounds(10, 185, 32, 32);
+        left.add(btn4);
+
+        JButton btn5 = new InsertionLeft(0);
+        btn5.setBounds(10, 300, 32, 32);
+        left.add(btn5);
+        
+        this.add(left, BorderLayout.WEST);
+        
+        JPanel right = new JPanel();
+        right.setBackground(new Color(68, 79, 116));
+        right.setPreferredSize(new Dimension(50, 500));
+        right.setLayout(null);
+
+        JButton btn6 = new InsertionRight(0);
+        btn6.setBounds(10, 71, 32, 32);
+        right.add(btn6);
+
+        JButton btn7 = new InsertionRight(0);
+        btn7.setBounds(10, 185, 32, 32);
+        right.add(btn7);
+
+        JButton btn8 = new InsertionRight(0);
+        btn8.setBounds(10, 300, 32, 32);
+        right.add(btn8);
+
+        this.add(right, BorderLayout.EAST);
+        
+        JPanel bottom = new JPanel();
+        bottom.setBackground(new Color(68, 79, 116));
+        bottom.setPreferredSize(new Dimension(500, 50));
+        bottom.setLayout(null);
+
+        JButton btn9 = new InsertionBottom(0);
+        btn9.setBounds(120, 10, 32, 32);
+        bottom.add(btn9);
+
+        JButton btn10 = new InsertionBottom(0);
+        btn10.setBounds(235, 10, 32, 32);
+        bottom.add(btn10);
+
+        JButton btn11 = new InsertionBottom(0);
+        btn11.setBounds(348, 10, 32, 32);
+        bottom.add(btn11);
+
+        this.add(bottom, BorderLayout.SOUTH);
+    }    
+}
